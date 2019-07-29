c45649c103b5f0db23cc42258326e31a2c07f30c
John Guest <jcguest90@gmail.com>
2019-07-29 22:43:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,10 +65,10 @@ def adding_matz
  
 
 	programmer_hash = {
- 		:yukihiro_matsumoto => {
-   :known_for => "Ruby",
-    :languages => ["LISP", "C"]
- }
+# 		:yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#     :languages => ["LISP", "C"]
+# }
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -82,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[]
     
 end
 
